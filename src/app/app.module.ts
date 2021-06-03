b623fb7d2f4d9242a3820a847a2cc0c41525f3f6
conklind <chad.conklin@das.cpa.com>
2021-06-02 20:12:40 -0700

    template section stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   imports: [
@@ -10,7 +12,10 @@ import { EventsListComponent } from './events/events-list.component';
   ],
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    NavBarComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    
   ],
   providers: [],
   bootstrap: [EventsAppComponent]
