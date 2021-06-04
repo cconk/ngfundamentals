95151cc4e2488aaeef7145c7b48b6c011da239b3
conklind <chad.conklin@das.cpa.com>
2021-06-03 20:17:58 -0700

    services updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrService } from './common/toastr.service';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
+
 
 @NgModule({
   imports: [
@@ -17,7 +20,10 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
